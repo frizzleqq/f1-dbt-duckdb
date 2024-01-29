d18ad50f74632efc5c4344cdbd40068c4ab316d0
frizzleqq <6024093+frizzleqq@users.noreply.github.com>
2024-01-29 11:08:07 +0100

    properly use env-var, add .env file

--- foneplatform/resources/__init__.py
+++ foneplatform/resources/__init__.py
@@ -1,9 +1,11 @@
+from dagster import EnvVar
+
 from .dbt_resource import dbt_resource
 from .ergast_resource import ErgastResource
 from .io_manager import LocalCsvIOManager
 
 default_resources = {
-    "io_manager": LocalCsvIOManager(),
+    "io_manager": LocalCsvIOManager(base_path=EnvVar("DATA_DIR")),
     "dbt": dbt_resource,
     "ergast": ErgastResource(),
 }
