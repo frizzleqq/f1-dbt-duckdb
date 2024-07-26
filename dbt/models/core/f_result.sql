2162da3e76ea550adab0c4100b6ce14b200603eb
Florian Fritz <flo.fritz@protonmail.com>
2024-07-26 21:34:31 +0200

    add more columns to f_results (#10)

--- dbt/models/core/f_result.sql
+++ dbt/models/core/f_result.sql
@@ -5,15 +5,10 @@ WITH results AS (
     FROM {{ ref('ergast_results') }}
 )
 
-, qualifying AS (
-    SELECT *
-    FROM {{ ref('ergast_qualifying') }}
-)
-
-, constructor_results AS (
-    SELECT *
-    FROM {{ ref('ergast_constructor_results') }}
-)
+-- , constructor_results AS (
+--     SELECT *
+--     FROM {{ ref('ergast_constructor_results') }}
+-- )
 
 , constructor_standings AS (
     SELECT *
@@ -23,6 +18,16 @@ WITH results AS (
 , driver_standings AS (
     SELECT *
     FROM {{ ref('ergast_driver_standings') }}
+)
+
+, qualifying AS (
+    SELECT *
+    FROM {{ ref('ergast_qualifying') }}
+)
+
+, result_status AS (
+    SELECT *
+    FROM {{ ref('ergast_status') }}
 )
 
 , d_circuit AS (
@@ -58,7 +63,7 @@ WITH results AS (
         , results.positiontext AS result_position_text
         , results.positionorder AS result_position_order
         , results.points AS result_points
-        -- , results.statusId
+        , result_status.status AS result_status
         , results.laps AS laps_completed
         , results.time AS finishing_time
         , results.milliseconds AS finishing_time_milliseconds
@@ -71,22 +76,29 @@ WITH results AS (
         , qualifying.q1 AS qualifying1_lap_time
         , qualifying.q2 AS qualifying2_lap_time
         , qualifying.q3 AS qualifying3_lap_time
-        -- TODO: constructor results
-        -- TODO: constructor standings
-        -- TODO: driver standings
+        , driver_standings.points AS driver_season_points
+        , driver_standings.driver_position AS driver_season_position
+        , driver_standings.positiontext AS driver_season_position_text
+        , driver_standings.wins AS driver_season_wins
+        , constructor_standings.points AS constructor_season_points
+        , constructor_standings.constructor_position AS constructor_season_position
+        , constructor_standings.positiontext AS constructor_season_position_text
+        , constructor_standings.wins AS constructor_season_wins
+        -- TODO: constructor results?
     FROM results
-    LEFT JOIN qualifying
-        ON qualifying.raceid = results.raceid AND qualifying.driverid = results.driverid
-    LEFT JOIN constructor_results
-        ON
-            constructor_results.raceid = results.raceid
-            AND constructor_results.constructorid = results.constructorid
+    LEFT JOIN result_status ON result_status.statusid = results.statusid
+    -- LEFT JOIN constructor_results
+    --     ON
+    --         constructor_results.raceid = results.raceid
+    --         AND constructor_results.constructorid = results.constructorid
     LEFT JOIN constructor_standings
         ON
             constructor_standings.raceid = results.raceid
             AND constructor_standings.constructorid = results.constructorid
     LEFT JOIN driver_standings
         ON driver_standings.raceid = results.raceid AND driver_standings.driverid = results.driverid
+    LEFT JOIN qualifying
+        ON qualifying.raceid = results.raceid AND qualifying.driverid = results.driverid
     LEFT JOIN d_race
         ON d_race.race_id = results.raceid
     LEFT JOIN d_circuit
